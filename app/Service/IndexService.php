3af412e2f4146144ae508c69578d9dc597f291b5
Alexzzl <805119233@qq.com>
2023-02-28 14:30:38 +0800

    国际化

--- app/Service/IndexService.php
+++ app/Service/IndexService.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Exception\BusinessException;
+use Hyperf\Contract\TranslatorInterface;
+use Hyperf\Utils\ApplicationContext;
 
 class IndexService
 {
     public function info(int $id)
     {
         if ($id <= 0) {
-            throw new BusinessException("id无效", 500);
+//            $translator = ApplicationContext::getContainer()->get(TranslatorInterface::class);
+//            $translator->setLocale('ja');
+            throw new BusinessException(trans('params.id_invalid'), 500);
         }
         return ['info' => 'data info'];
     }
---
+++ storage/languages/ja/params.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'id_invalid' => 'ID が無効です',
+];
