5a824e21be034bc4d1effe42eec419b360b93e76
Alexzzl <805119233@qq.com>
2023-02-28 14:34:18 +0800

    国际化

--- app/Middleware/GlobalMiddleware.php
+++ app/Middleware/GlobalMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use Hyperf\Contract\TranslatorInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -12,12 +13,22 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class GlobalMiddleware implements MiddlewareInterface
 {
-    public function __construct(protected ContainerInterface $container)
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    public function __construct(ContainerInterface $container, TranslatorInterface $translator)
     {
+        $this->translator = $translator;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        if ($lang = $request->getHeaderLine('lang')) {
+            $this->translator->setLocale($lang);
+        }
+
         return $handler->handle($request);
     }
 }
